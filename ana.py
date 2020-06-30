21c5868c87981dfb5a743b908dafacec21cc36b4
JN513 <jn5.guape@gmail.com>
2020-06-30 16:01:21 -0300

    criando função para criação de audios e leitor aud

--- ana.py
+++ ana.py
@@ -8,12 +8,11 @@ def monitora_audio():
         print("Estou aqui!!! Pode falar.")
         audio = microfone.listen(source)
 
-    # recognize speech using Google Cloud Speech
-    GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-    try:
-        print(microfone.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-    except sr.UnknownValueError:
-        print("Google Cloud Speech could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+        try:
+            print("Voce disse " + microfone.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+
 monitora_audio()
--- cria_Audio.py
+++ cria_Audio.py
@@ -6,10 +6,10 @@ def cria_audio(audio,nome):
     tts = gTTS(audio, lang='pt-br')         #a menssagem do audio e a linguagem
     tts.save('audios/'+nome+'.mp3')         #salvando o audio
     #sinataxe o player e o audio
-    call(['aplay','audios/'+nome+'.mp3'])   #no linux
+    call(['ffplay','-nodisp','-autoexit','audios/'+nome+'.mp3'])   #no linux
     #call(['afplay','audios/hello.mp3'])    #no mac
     #playsound('audios/hello.mp3')          #windows aqui so passa o audio
 
-a = input('Digite a menssage do audio:')
-b = input('Digite o nome d audio:')
+a = input('Digite a menssage do audio:\n')
+b = input('Digite o nome do audio:\n')
 cria_audio(a,b)
