f4e0220b902db782b12e95f699b27b662bc168a3
JN513 <jn5.guape@gmail.com>
2020-06-30 17:16:15 -0300

    credenciais

--- ana.py
+++ ana.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import speech_recognition as sr
 
+#####config#####
+with open('sistema-intelige-1537185170980-b86ebc917520.json') as credenciais_google:
+    credenciais_google = credenciais_google.read()
+
+
 def monitora_audio():
     microfone = sr.Recognizer() #pega o adio do microfone
 
@@ -8,11 +13,12 @@ def monitora_audio():
         print("Estou aqui!!! Pode falar.")
         audio = microfone.listen(source)
 
-        try:
-            print("Voce disse " + microfone.recognize_sphinx(audio))
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+    try:
+        print("Voce disse" + microfone.recognize_google_cloud(audio, credentials_json=credenciais_google, language='pt-BR'))
+    except sr.UnknownValueError:
+        print("Não consegui ouvir o audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
 
 monitora_audio()
