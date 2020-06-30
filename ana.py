00f2ed356be0457b3fe999080c74727e975af557
JN513 <jn5.guape@gmail.com>
2020-06-30 20:39:55 -0300

    retornado feedback e organizando diretorio

--- ana.py
+++ ana.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import speech_recognition as sr
+from subprocess import call
 
 ##### configurações #####
+
 hotword = 'ana'
 with open('config/sistema-intelige-1537185170980-b86ebc917520.json') as credenciais_google:
     credenciais_google = credenciais_google.read()
@@ -12,20 +14,32 @@ with open('config/sistema-intelige-1537185170980-b86ebc917520.json') as credenci
 def monitora_audio():
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
+
         while True:
             print("Estou aqui: ")
             audio = microfone.listen(source)
+
             try:
+
                 trigger = microfone.recognize_google_cloud(audio, credentials_json=credenciais_google, language='pt-BR')
                 trigger = trigger.lower()
+
                 if hotword in trigger:
                     print('Comando: ', trigger)
+                    responde('feedback')
                     ### fazer algo ###
                     break
+
             except sr.UnknownValueError:
                 print("Google not understand audio")
+
             except sr.RequestError as e:
                 print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+    return trigger   
+
+def responde(arquivo):
+    call(['ffplay','-nodisp','-autoexit','audios/'+arquivo+'.mp3'])
 
 
 monitora_audio()
