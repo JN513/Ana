215ef2d121c346ca94da6088da40335ab8883d29
JN513 <jn5.guape@gmail.com>
2020-06-30 20:28:18 -0300

    criando trigger

--- ana.py
+++ ana.py
@@ -1,23 +1,31 @@
 # -*- coding: utf-8 -*-
 import speech_recognition as sr
 
-#####config#####
-with open('sistema-intelige-1537185170980-b86ebc917520.json') as credenciais_google:
+##### configurações #####
+hotword = 'ana'
+with open('config/sistema-intelige-1537185170980-b86ebc917520.json') as credenciais_google:
     credenciais_google = credenciais_google.read()
 
+
+##### funções principais #####
+
 def monitora_audio():
-    microfone = sr.Recognizer() #pega o adio do microfone
-
+    microfone = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Estou aqui!!! Pode falar.")
-        audio = microfone.listen(source)
-
-    try:
-        print("Voce disse" + microfone.recognize_google_cloud(audio, credentials_json=credenciais_google, language='pt-BR'))
-    except sr.UnknownValueError:
-        print("Não consegui ouvir o audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+        while True:
+            print("Estou aqui: ")
+            audio = microfone.listen(source)
+            try:
+                trigger = microfone.recognize_google_cloud(audio, credentials_json=credenciais_google, language='pt-BR')
+                trigger = trigger.lower()
+                if hotword in trigger:
+                    print('Comando: ', trigger)
+                    ### fazer algo ###
+                    break
+            except sr.UnknownValueError:
+                print("Google not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Cloud Speech service; {0}".format(e))
 
 
 monitora_audio()
