8815305d7d158dd203389d367f48d19c4d25b607
JN513 <jn5.guape@gmail.com>
2020-06-30 23:25:21 -0300

    add funções como abrir paginas

--- ana.py
+++ ana.py
@@ -5,6 +5,7 @@ from datetime import time, datetime, timezone
 from subprocess import call
 from requests import get
 from bs4 import BeautifulSoup
+import webbrowser as browser
 #from playsound import playsound            #para reproduzir no windows
 
 ##### configurações #####
@@ -33,7 +34,7 @@ def monitora_audio():
                     print('Comando: ', trigger)
                     responde('feedback')
                     executa_comandos(trigger)
-                    break
+                    #break
 
             except sr.UnknownValueError:
                 print("Google not understand audio")
@@ -61,6 +62,15 @@ def executa_comandos(trigger):
         ultimas_noticias()
     elif 'hora' in trigger:
         hora()
+    elif 'toca' in trigger:
+        album = trigger.strip(hotword)
+        playlist(album)
+    elif 'abra' in trigger:
+        nome = trigger.strip(hotword)
+        abre(nome)
+    elif 'abrir' in trigger:
+        nome = trigger.strip(hotword)
+        abre(nome)
     else:
         menssagem = trigger.strip(hotword)
         cria_audio(menssagem)
@@ -82,7 +92,23 @@ def hora():
     data_e_hora_em_texto = data_e_hora_atuais.strftime('%d/%m/%Y %H:%M')
     cria_audio(data_e_hora_em_texto)
 
-def playlist():
+def playlist(album):
+    if 'agora' in album:
+        browser.open_new_tab('https://open.spotify.com/track/1ARaDhugzpyiEeuIXswY4W?si=XIGUAnDIQ1yh93_mVNVPBg') #so o open abre janela
+    elif 'flow' in album:
+        browser.open_new_tab('https://open.spotify.com/episode/6t1EnKWAAziEiu98NBGLCu')
+    elif 'rock' in album:
+        browser.open_new_tab('https://open.spotify.com/track/1Mf27cnAF1Q6Ko83XTM5d1?si=f1LVtboZS3CeF6LBbQzI7A')
+    elif 'pop' in album:
+        browser.open_new_tab('https://open.spotify.com/track/738SQjONa0q63yhLxvg3m1?si=gzQctUONSh2PU-O3PIFMUg')
+    else:
+        browser.open_new_tab('https://open.spotify.com/track/4u7EnebtmKWzUH433cf5Qv?si=QABP8IHuQv-Bg9R7qtC8Fg')
+
+def abre(nome):
+    if 'whatsapp' in nome:
+        browser.open_new_tab('https://web.whatsapp.com/')
+    elif 'github' in nome:
+        browser.open_new_tab('https://github.com/JN513')
 
 ##### função inicio #####
 
