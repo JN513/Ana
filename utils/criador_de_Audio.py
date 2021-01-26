2118f38e79b2e73e59ced07b7755d05ec4a8ec82
Julio <jn5.guape@gmail.com>
2021-01-26 14:03:01 -0300

    update criador de audio

--- utils/criador_de_Audio.py
+++ utils/criador_de_Audio.py
@@ -1,18 +1,22 @@
 from gtts import gTTS  # biblioteca para criação de audio e tals
 from subprocess import call  # para reproduzir audio no linux e mac
+import vlc
 
 # from playsound import playsound            #para reproduzir no windows
 
 
 def cria_audio(audio, nome):
     tts = gTTS(audio, lang="pt-br")  # a menssagem do audio e a linguagem
-    tts.save("audios/" + nome + ".mp3")  # salvando o audio
+    tts.save(f"audios/{nome}.mp3")  # salvando o audio
     # sinataxe o player e o audio
-    call(["ffplay", "-nodisp", "-autoexit", "audios/" + nome + ".mp3"])  # no linux
+    # call(["ffplay", "-nodisp", "-autoexit", "audios/" + nome + ".mp3"])  # no linux
+    p = vlc.MediaPlayer(f"audios/{nome}.mp3")
+    p.play()
     # call(['afplay','audios/'+nome+'.mp3'])    #no mac
     # playsound('audios/'+nome+'.mp3')          #windows aqui so passa o audio
 
 
 a = input("Digite a menssage do audio:\n")
 b = input("Digite o nome do audio:\n")
+
 cria_audio(a, b)
